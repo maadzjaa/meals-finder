731f5bac995eef9276c8202fbc66617993fc4ece
maadzjaa <magda.g2112@gmail.com>
2024-03-07 14:18:37 +0100

    refactor: categories link list

--- src/components/organisms/CategoriesLinkList.tsx
+++ src/components/organisms/CategoriesLinkList.tsx
@@ -1,31 +1,17 @@
-import { useState, useEffect } from 'react';
-import { Category } from '../../types';
-import { fetchData } from '../../utils';
 import { Link } from 'react-router-dom';
 import './CategoriesLinkList.css';
 
-const categoriesMealUrl = 'https://www.themealdb.com/api/json/v1/1/list.php?c=list';
-
 export function CategoriesLinkList() {
-	const [categories, setCategories] = useState<Category[] | null>(null);
-
-	useEffect(() => {
-		const fetchCategories = async () => {
-			const data = await fetchData(categoriesMealUrl);
-			const mealCategories = data.meals;
-			setCategories(mealCategories);
-		};
-		fetchCategories();
-	}, []);
+	const categories = ['Beef', 'Breakfast', 'Chicken', 'Dessert', 'Goat', 'Lamb', 'Miscellaneous', 'Pasta', 'Pork', 'Seafood', 'Side', 'Starter', 'Vegan', 'Vegetarian'];
 
 	return (
 		<ul className='category-link-list'>
 			{categories &&
 				categories.map((category) => {
 					return (
-						<li key={category.strCategory}>
-							<Link className='category-link-list-link' to={`/categories/${category.strCategory.toLowerCase()}`}>
-								{category.strCategory}
+						<li key={category}>
+							<Link className='category-link-list-link' to={`/categories/${category.toLowerCase()}`}>
+								{category}
 							</Link>
 						</li>
 					);
